71f799b71e7117cfd98cfda6456e7363c0d798d7
Tesfa <tesfamichaelalimaw26@gmail.com>
2026-01-26 15:39:38 +0300

    admin can add,edit,delete and update category of items

--- frontend/src/services/category.service.js
+++ frontend/src/services/category.service.js
@@ -9,6 +9,21 @@ const categoryService = {
   getCategoryById: async (id) => {
     const response = await api.get(`/categories/${id}`);
     return response.data;
+  },
+
+  createCategory: async (data) => {
+    const response = await api.post('/categories', data);
+    return response.data;
+  },
+
+  updateCategory: async (id, data) => {
+    const response = await api.patch(`/categories/${id}`, data);
+    return response.data;
+  },
+
+  deleteCategory: async (id) => {
+    const response = await api.delete(`/categories/${id}`);
+    return response.data;
   }
 };
 
